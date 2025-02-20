884fab291f3a50a9498904d0ef2e0be9d7b1fc56
Péter Kardos <kardospeter1994@hotmail.com>
2025-02-20 17:35:27 +0100

    add strongly typed UIDs

--- sed_manager/src/messaging/uid.rs
+++ sed_manager/src/messaging/uid.rs
@@ -6,6 +6,12 @@ pub struct UID {
     object: u32,
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct TableUID(UID);
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct ObjectUID<const TABLE: u64>(UID);
+
 impl UID {
     pub const fn null() -> Self {
         Self::new(0)
@@ -19,6 +25,10 @@ impl UID {
         ((self.table as u64) << 32) | (self.object as u64)
     }
 
+    pub const fn as_uid(&self) -> UID {
+        *self
+    }
+
     pub const fn is_table(&self) -> bool {
         self.table != 0 && self.object == 0
     }
@@ -28,7 +38,11 @@ impl UID {
     }
 
     pub const fn is_object(&self) -> bool {
-        self.object != 0
+        self.table != 0 && self.object != 0
+    }
+
+    pub const fn is_special(&self) -> bool {
+        self.table == 0
     }
 
     pub const fn to_descriptor(&self) -> Option<Self> {
@@ -56,6 +70,84 @@ impl UID {
     }
 }
 
+impl TableUID {
+    const fn is_value_accepted(value: u64) -> bool {
+        let value_uid = UID::new(value);
+        value_uid.is_table()
+    }
+
+    pub const fn new(value: u64) -> Self {
+        assert!(Self::is_value_accepted(value));
+        Self(UID::new(value))
+    }
+
+    pub const fn try_new(value: u64) -> Result<Self, u64> {
+        if Self::is_value_accepted(value) {
+            Ok(Self(UID::new(value)))
+        } else {
+            Err(value)
+        }
+    }
+
+    pub const fn as_u64(&self) -> u64 {
+        self.0.as_u64()
+    }
+
+    pub const fn as_uid(&self) -> UID {
+        self.0
+    }
+
+    pub const fn to_descriptor(&self) -> ObjectUID<1> {
+        ObjectUID(self.0.to_descriptor().unwrap())
+    }
+}
+
+impl<const TABLE: u64> ObjectUID<TABLE> {
+    const fn is_value_accepted(value: u64) -> bool {
+        let table_uid = UID::new(TABLE);
+        let value_uid = UID::new(value);
+        assert!(table_uid.is_table());
+        table_uid.table == value_uid.table && value_uid.is_object()
+    }
+
+    pub const fn new(value: u64) -> Self {
+        assert!(Self::is_value_accepted(value));
+        Self(UID::new(value))
+    }
+
+    pub const fn try_new(value: u64) -> Result<Self, u64> {
+        if Self::is_value_accepted(value) {
+            Ok(Self(UID::new(value)))
+        } else {
+            Err(value)
+        }
+    }
+
+    pub const fn as_u64(&self) -> u64 {
+        self.0.as_u64()
+    }
+
+    pub const fn as_uid(&self) -> UID {
+        self.0
+    }
+
+    pub const fn is_descriptor(&self) -> bool {
+        self.0.is_descriptor()
+    }
+
+    pub const fn to_table(&self) -> Option<TableUID> {
+        if self.is_descriptor() {
+            Some(TableUID(UID { table: self.0.object, object: 0 }))
+        } else {
+            None
+        }
+    }
+
+    pub const fn containing_table(&self) -> TableUID {
+        TableUID::new(TABLE)
+    }
+}
+
 impl From<UID> for Value {
     fn from(value: UID) -> Self {
         Value::from(Bytes::from(value.as_u64().to_be_bytes()))
@@ -65,11 +157,8 @@ impl From<UID> for Value {
 impl TryFrom<Value> for UID {
     type Error = Value;
     fn try_from(value: Value) -> Result<Self, Self::Error> {
-        match <&Bytes>::try_from(&value) {
-            Ok(bytes) => match <[u8; 8]>::try_from(bytes.as_slice()) {
-                Ok(fixed_bytes) => Ok(Self::new(u64::from_be_bytes(fixed_bytes))),
-                Err(_) => Err(value),
-            },
+        match <&[u8; 8]>::try_from(&value) {
+            Ok(bytes) => Ok(Self::new(u64::from_be_bytes(*bytes))),
             Err(_) => Err(value),
         }
     }
@@ -89,54 +178,177 @@ impl From<UID> for u64 {
 
 impl Default for UID {
     fn default() -> Self {
-        UID::null()
+        Self::null()
+    }
+}
+
+impl From<TableUID> for Value {
+    fn from(value: TableUID) -> Self {
+        Value::from(value.as_uid())
+    }
+}
+
+impl TryFrom<Value> for TableUID {
+    type Error = Value;
+    fn try_from(value: Value) -> Result<Self, Self::Error> {
+        match <&[u8; 8]>::try_from(&value) {
+            Ok(bytes) => match Self::try_new(u64::from_be_bytes(*bytes)) {
+                Ok(uid) => Ok(uid),
+                Err(_) => Err(value),
+            },
+            Err(_) => Err(value),
+        }
+    }
+}
+
+impl TryFrom<u64> for TableUID {
+    type Error = u64;
+    fn try_from(value: u64) -> Result<Self, Self::Error> {
+        Self::try_new(value)
+    }
+}
+
+impl From<TableUID> for u64 {
+    fn from(value: TableUID) -> Self {
+        value.as_u64()
+    }
+}
+
+impl TryFrom<UID> for TableUID {
+    type Error = UID;
+    fn try_from(value: UID) -> Result<Self, Self::Error> {
+        Self::try_new(value.as_u64()).map_err(|_| value)
+    }
+}
+
+impl From<TableUID> for UID {
+    fn from(value: TableUID) -> Self {
+        value.0
+    }
+}
+
+impl<const TABLE: u64> From<ObjectUID<TABLE>> for Value {
+    fn from(value: ObjectUID<TABLE>) -> Self {
+        Value::from(value.as_uid())
+    }
+}
+
+impl<const TABLE: u64> TryFrom<Value> for ObjectUID<TABLE> {
+    type Error = Value;
+    fn try_from(value: Value) -> Result<Self, Self::Error> {
+        match <&[u8; 8]>::try_from(&value) {
+            Ok(bytes) => match Self::try_new(u64::from_be_bytes(*bytes)) {
+                Ok(uid) => Ok(uid),
+                Err(_) => Err(value),
+            },
+            Err(_) => Err(value),
+        }
+    }
+}
+
+impl<const TABLE: u64> TryFrom<u64> for ObjectUID<TABLE> {
+    type Error = u64;
+    fn try_from(value: u64) -> Result<Self, Self::Error> {
+        Self::try_new(value)
+    }
+}
+
+impl<const TABLE: u64> From<ObjectUID<TABLE>> for u64 {
+    fn from(value: ObjectUID<TABLE>) -> Self {
+        value.as_u64()
+    }
+}
+
+impl<const TABLE: u64> TryFrom<UID> for ObjectUID<TABLE> {
+    type Error = UID;
+    fn try_from(value: UID) -> Result<Self, Self::Error> {
+        Self::try_new(value.as_u64()).map_err(|_| value)
+    }
+}
+
+impl<const TABLE: u64> From<ObjectUID<TABLE>> for UID {
+    fn from(value: ObjectUID<TABLE>) -> Self {
+        value.0
     }
 }
 
 impl std::fmt::Debug for UID {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "UID( {:8x}_{:8x} )", self.table, self.object)
+        write!(f, "UID::{:#010x}_{:08x}", self.table, self.object)
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
 
+    use super::*;
     const TABLE: UID = UID::new(0x0000_0001_0000_0000);
     const DESCRIPTOR: UID = UID::new(0x0000_0001_0000_0001);
     const OBJECT: UID = UID::new(0x0000_0009_0000_0001);
-    const CONTAINING: UID = UID::new(0x0000_0009_0000_0000);
+    const SOME_TABLE: UID = UID::new(0x0000_0009_0000_0000);
     const SM_METHOD: UID = UID::new(0x0000_0000_0000_FF01);
 
     #[test]
-    fn determine_uid_type() {
+    fn uid_check_uid_kind() {
         assert!(TABLE.is_table());
         assert!(!TABLE.is_descriptor());
         assert!(!TABLE.is_object());
+        assert!(!TABLE.is_special());
 
         assert!(!DESCRIPTOR.is_table());
         assert!(DESCRIPTOR.is_descriptor());
         assert!(DESCRIPTOR.is_object());
+        assert!(!DESCRIPTOR.is_special());
 
         assert!(!OBJECT.is_table());
         assert!(!OBJECT.is_descriptor());
         assert!(OBJECT.is_object());
+        assert!(!OBJECT.is_special());
 
         assert!(!SM_METHOD.is_table());
         assert!(!SM_METHOD.is_descriptor());
-        assert!(SM_METHOD.is_object());
+        assert!(!SM_METHOD.is_object());
+        assert!(SM_METHOD.is_special());
     }
 
     #[test]
-    fn convert_table_and_descriptor() {
-        assert_eq!(TABLE.to_descriptor().unwrap(), DESCRIPTOR);
-        assert_eq!(TABLE, DESCRIPTOR.to_table().unwrap());
+    fn uid_to_descriptor() {
+        assert_eq!(TABLE.to_descriptor(), Some(DESCRIPTOR));
+        assert_eq!(DESCRIPTOR.to_descriptor(), None);
+        assert_eq!(OBJECT.to_descriptor(), None);
+        assert_eq!(SM_METHOD.to_descriptor(), None);
     }
 
     #[test]
-    fn get_containing_table() {
-        assert_eq!(OBJECT.containing_table().unwrap(), CONTAINING);
-        assert_eq!(DESCRIPTOR.containing_table().unwrap(), TABLE);
+    fn uid_to_table() {
+        assert_eq!(TABLE.to_table(), None);
+        assert_eq!(DESCRIPTOR.to_table(), Some(TABLE));
+        assert_eq!(OBJECT.to_table(), None);
+        assert_eq!(SM_METHOD.to_table(), None);
+    }
+
+    #[test]
+    fn uid_containing_table() {
+        assert_eq!(TABLE.containing_table(), None);
+        assert_eq!(DESCRIPTOR.containing_table(), Some(TABLE));
+        assert_eq!(OBJECT.containing_table(), Some(SOME_TABLE));
+        assert_eq!(SM_METHOD.containing_table(), None);
+    }
+
+    #[test]
+    fn table_uid_try_new() {
+        assert_eq!(TableUID::try_from(TABLE), Ok(TableUID::new(TABLE.as_u64())));
+        assert_eq!(TableUID::try_from(DESCRIPTOR), Err(DESCRIPTOR));
+        assert_eq!(TableUID::try_from(OBJECT), Err(OBJECT));
+        assert_eq!(TableUID::try_from(SM_METHOD), Err(SM_METHOD));
+    }
+
+    #[test]
+    fn object_uid_try_new() {
+        type SomeTableUID = ObjectUID<{ SOME_TABLE.as_u64() }>;
+        assert_eq!(SomeTableUID::try_from(TABLE), Err(TABLE));
+        assert_eq!(SomeTableUID::try_from(DESCRIPTOR), Err(DESCRIPTOR));
+        assert_eq!(SomeTableUID::try_from(OBJECT), Ok(SomeTableUID::new(OBJECT.as_u64())));
+        assert_eq!(SomeTableUID::try_from(SM_METHOD), Err(SM_METHOD));
     }
 }
--- sed_manager/src/messaging/uid_range.rs
+++ sed_manager/src/messaging/uid_range.rs
@@ -1,4 +1,4 @@
-use super::uid::UID;
+use super::uid::{ObjectUID, UID};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct UIDRange {
@@ -6,6 +6,9 @@ pub struct UIDRange {
     end: UID,
     step: u64,
 }
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct ObjectUIDRange<const TABLE: u64>(UIDRange);
 
 impl UIDRange {
     pub const fn new_range(start: UID, end: UID, step: u64) -> Self {
@@ -39,9 +42,56 @@ impl UIDRange {
     }
 }
 
+impl<const TABLE: u64> ObjectUIDRange<TABLE> {
+    pub const fn new_range(start: ObjectUID<TABLE>, end: ObjectUID<TABLE>, step: u64) -> Self {
+        Self(UIDRange::new_range(start.as_uid(), end.as_uid(), step))
+    }
+
+    pub const fn new_count(start: ObjectUID<TABLE>, count: u64, step: u64) -> Self {
+        Self(UIDRange::new_count(start.as_uid(), count, step))
+    }
+
+    pub const fn nth(&self, offset: u64) -> Option<ObjectUID<TABLE>> {
+        if let Some(uid) = self.0.nth(offset) {
+            Some(ObjectUID::<TABLE>::new(uid.as_u64()))
+        } else {
+            None
+        }
+    }
+
+    pub const fn contains(&self, uid: ObjectUID<TABLE>) -> bool {
+        self.0.contains(uid.as_uid())
+    }
+
+    pub const fn index_of(&self, uid: ObjectUID<TABLE>) -> Option<u64> {
+        self.0.index_of(uid.as_uid())
+    }
+
+    pub const fn as_uid_range(&self) -> UIDRange {
+        self.0
+    }
+}
+
 impl From<UID> for UIDRange {
     fn from(value: UID) -> Self {
         Self::new_count(value, 1, 1)
+    }
+}
+
+impl<const TABLE: u64> From<ObjectUID<TABLE>> for ObjectUIDRange<TABLE> {
+    fn from(value: ObjectUID<TABLE>) -> Self {
+        Self::new_count(value, 1, 1)
+    }
+}
+
+impl<const TABLE: u64> TryFrom<UIDRange> for ObjectUIDRange<TABLE> {
+    type Error = UIDRange;
+    fn try_from(value: UIDRange) -> Result<Self, Self::Error> {
+        if let (Ok(_), Ok(_)) = (ObjectUID::<TABLE>::try_from(value.start), ObjectUID::<TABLE>::try_from(value.end)) {
+            Ok(Self(value))
+        } else {
+            Err(value)
+        }
     }
 }
 
