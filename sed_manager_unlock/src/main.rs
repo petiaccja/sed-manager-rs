7546ce2c439adac433360c57b8f4ddb1f97862ee
Péter Kardos <kardospeter1994@hotmail.com>
2025-03-17 23:15:49 +0100

    print current devices

--- sed_manager_unlock/src/main.rs
+++ sed_manager_unlock/src/main.rs
@@ -1,4 +1,97 @@
-use std::io::{self, Write};
+use std::io;
+use std::sync::Arc;
+
+use sed_manager::device::{list_physical_drives, open_device, Device, Error as DeviceError};
+use sed_manager::messaging::discovery::LockingDescriptor;
+use sed_manager::rpc::discover;
+
+struct DeviceList {
+    shadowed: Vec<Arc<dyn Device>>,
+    locked: Vec<Arc<dyn Device>>,
+    other: Vec<Box<dyn Device>>,
+    failed: Vec<(String, DeviceError)>,
+}
+
+fn get_device_list() -> Result<DeviceList, DeviceError> {
+    let mut device_list = DeviceList::new();
+    let paths = list_physical_drives()?;
+    for path in paths {
+        let device = match open_device(&path) {
+            Ok(device) => device,
+            Err(error) => {
+                device_list.failed.push((path, error));
+                continue;
+            }
+        };
+        let Ok(discovery) = discover(&*device) else {
+            device_list.other.push(device);
+            continue;
+        };
+        let Some(locking_desc) = discovery.get::<LockingDescriptor>() else {
+            device_list.other.push(device);
+            continue;
+        };
+        if !locking_desc.mbr_done && locking_desc.mbr_enabled {
+            device_list.shadowed.push(Arc::from(device));
+        } else if locking_desc.locked {
+            device_list.locked.push(Arc::from(device));
+        } else {
+            device_list.other.push(device);
+        }
+    }
+    Ok(device_list)
+}
+
+fn print_devices(device_list: &DeviceList) -> Result<(), DeviceError> {
+    println!("Shadowed devices:");
+    for device in &device_list.shadowed {
+        println!("  {} / {}", device.model_number()?, device.serial_number()?);
+    }
+
+    println!("\nLocked devices:");
+    for device in &device_list.locked {
+        println!("  {} / {}", device.model_number()?, device.serial_number()?);
+    }
+
+    println!("\nOther devices:");
+    for device in &device_list.other {
+        println!("  {} / {}", device.model_number()?, device.serial_number()?);
+    }
+
+    println!("\nFailed devices:");
+    for device in &device_list.failed {
+        println!("  {} / {}", device.0, device.1);
+    }
+
+    Ok(())
+}
+
+fn main() -> io::Result<()> {
+    println!("{BANNER}{VERSION}\n\n");
+
+    let device_list = match get_device_list() {
+        Ok(device_list) => device_list,
+        Err(error) => {
+            eprintln!("Failed to list devices:\n  {error}");
+            return Ok(());
+        }
+    };
+
+    let _ = print_devices(&device_list);
+
+    println!("\nPress enter to exit...");
+    let stdin = io::stdin();
+    let mut buffer = String::new();
+    stdin.read_line(&mut buffer)?;
+
+    Ok(())
+}
+
+impl DeviceList {
+    pub fn new() -> Self {
+        Self { shadowed: vec![], locked: vec![], other: vec![], failed: vec![] }
+    }
+}
 
 const BANNER: &str = r"
      ____  _____ ____  __  __                                   
@@ -10,16 +103,3 @@ const BANNER: &str = r"
                               v";
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
-
-fn main() -> io::Result<()> {
-    let mut buffer = String::new();
-    let mut stdout = io::stdout();
-    stdout.write(BANNER.as_bytes())?;
-    stdout.write(VERSION.as_bytes())?;
-    stdout.write("\n\n".as_bytes())?;
-    stdout.write("Press enter to exit: ".as_bytes())?;
-    stdout.flush()?;
-    let stdin = io::stdin();
-    stdin.read_line(&mut buffer)?;
-    Ok(())
-}
