7e3f666fbc4dad9d3d51e519baeff6789d235f27
Péter Kardos <kardospeter1994@hotmail.com>
2025-03-25 01:13:05 +0100

    add license popup & settings base (#20)

---
+++ sed_manager_config/src/logging.rs
@@ -0,0 +1,24 @@
+//L-----------------------------------------------------------------------------
+//L Copyright (C) Péter Kardos
+//L Please refer to the full license distributed with this software.
+//L-----------------------------------------------------------------------------
+
+use std::fs::File;
+
+pub fn init() -> Option<tracing_appender::non_blocking::WorkerGuard> {
+    let temp_dir = std::env::temp_dir();
+    let log_file_path = temp_dir.join("sed-manager.log");
+    if let Ok(file) = File::create(log_file_path.as_path()) {
+        let (non_blocking, guard) = tracing_appender::non_blocking(file);
+        let subscriber = tracing_subscriber::fmt()
+            .with_ansi(false)
+            .with_writer(non_blocking)
+            .with_max_level(tracing::Level::DEBUG)
+            .with_target(false);
+        let _ = tracing::subscriber::set_global_default(subscriber.finish());
+        Some(guard)
+    } else {
+        eprintln!("Could not write to log file: {}", log_file_path.as_path().to_str().unwrap_or("?"));
+        None
+    }
+}
